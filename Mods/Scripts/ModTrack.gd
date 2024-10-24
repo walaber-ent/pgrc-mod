f59768d213603cd610668789f7b8d9bda91ea80c
Tim FitzRandolph <walaber@walaber.com>
2024-10-24 15:43:39 -0700

    barriers, removed .blend
    - removed the track .blend file so this basic template doesn't have a blender dependency
    - added placeholder assets and scripts for adding arrow barriers to tracks

--- Mods/Scripts/ModTrack.gd
+++ Mods/Scripts/ModTrack.gd
@@ -5,6 +5,7 @@ class_name ModTrack
 @onready var car_spawn: Marker3D = %CarSpawn
 @onready var checkpoints: Node3D = %Checkpoints
 @onready var track_path: ModTrackPath = %TrackPath
+@onready var barriers: Node3D = %Barriers
 @onready var out_of_bounds: Node3D = %OutOfBounds
 @onready var spectator_cars: Node3D = %SpectatorCars
 @onready var track_cameras: Node3D = %TrackCameras
